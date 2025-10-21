017996ad8157745257c60b2a1a67292103de1aa6
Abner Daniel Fuentes Juárez <abnerfuentes05@gmail.com>
2025-10-20 18:42:59 -0600

    Refactor orders table migration for improved readability

--- database/migrations/2025_10_21_003206_create_orders_table.php
+++ database/migrations/2025_10_21_003206_create_orders_table.php
@@ -11,22 +11,20 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
-            Schema::create('orders', function (Blueprint $table) {
-                $table->id();
-                $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
-                $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete(); // quién crea/entrega
-                $table->string('code', 40)->unique(); // folio/serie si lo necesitas
-                $table->enum('status', ['draft', 'confirmed', 'shipped', 'delivered', 'canceled'])->default('confirmed');
-                $table->enum('payment_status', ['unpaid', 'partial', 'paid'])->default('unpaid');
-                $table->dateTime('delivered_at')->nullable();
-                $table->decimal('subtotal', 14, 4)->default(0);
-                $table->decimal('discount_total', 14, 4)->default(0);
-                $table->decimal('tax_total', 14, 4)->default(0);
-                $table->decimal('grand_total', 14, 4)->default(0);
-                $table->text('notes')->nullable();
-                $table->timestamps();
-                $table->index(['status', 'payment_status', 'delivered_at']);
-            });
+            $table->id();
+            $table->foreignId('customer_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete(); // quién crea/entrega
+            $table->string('code', 40)->unique(); // folio/serie si lo necesitas
+            $table->enum('status', ['draft', 'confirmed', 'shipped', 'delivered', 'canceled'])->default('confirmed');
+            $table->enum('payment_status', ['unpaid', 'partial', 'paid'])->default('unpaid');
+            $table->dateTime('delivered_at')->nullable();
+            $table->decimal('subtotal', 14, 4)->default(0);
+            $table->decimal('discount_total', 14, 4)->default(0);
+            $table->decimal('tax_total', 14, 4)->default(0);
+            $table->decimal('grand_total', 14, 4)->default(0);
+            $table->text('notes')->nullable();
+            $table->timestamps();
+            $table->index(['status', 'payment_status', 'delivered_at']);
         });
     }
 
